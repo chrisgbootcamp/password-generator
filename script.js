2738f1f2519388f241d4078215e784e57fef2081
Aldana <chris.bootcampmail@gmail.com>
2020-05-08 18:35:58 -0400

    added things for machine transfer

---
+++ script.js
@@ -0,0 +1,6 @@
+function generate(){
+    let complexity=document.getElementById("slider").nodeValue
+    let values="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*()_+-="
+    let password= "";
+    for(var=0; i<=complexity;i++;()
+    }
